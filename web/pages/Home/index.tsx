1b51a37b7a2da69c0917387ccd31696672caeca9
Alexander Kruk <alexander.kruk@gmail.com>
2025-05-20 21:08:24 +0200

    updated name

--- web/pages/Home/index.tsx
+++ web/pages/Home/index.tsx
@@ -84,7 +84,7 @@ const HomePage: Component = () => {
           aria-labelledby="homeTitle"
         >
           <span id="homeTitle" aria-hidden="true">
-            Agn<span class="text-[var(--hl-500)]">ai</span>
+            Porn<span class="text-[var(--hl-500)]">AI</span>
           </span>
         </div>
 
